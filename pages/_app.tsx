61ad2a8d6b3ab6d0b56dd89a36d48f482ee62010
Andrew van Rooyen <wraith.andrew@gmail.com>
2021-04-26 14:47:03 +0200

    feat(font): add Roboto

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import '../styles/font.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Roboto', 'sans-serif']
+      }
+    },
     colors: {
       base: {
         '1': colors.coolGray[200],
