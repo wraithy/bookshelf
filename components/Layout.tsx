0d237fea4bd7adbe24a45fc332371ce0167dc6f8
Andrew van Rooyen <wraith.andrew@gmail.com>
2021-06-15 11:26:26 +0200

    feat(Layout): add context for nav states. hide seriesgraph while transitioning

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Nav from 'components/Nav'
 import { useWindowSize } from '../util'
 
+interface LayoutContextState {
+  navOpen: boolean
+  isTransitioning: boolean
+}
+
+export const LayoutContext = React.createContext<LayoutContextState>({
+  navOpen: true,
+  isTransitioning: false,
+})
+
+const transitionDurationMs = 150
+
 export default function Layout({ children, contentClass = '' }: { children: any; contentClass?: string }) {
   const [navOpen, setNavOpen] = useState(true)
+  const [isTransitioning, setIsTransitioning] = useState(false)
   const { isMobile } = useWindowSize()
+
+  const toggleNavOpen = () => {
+    setNavOpen(!navOpen)
+    setIsTransitioning(true)
+    setTimeout(() => setIsTransitioning(false), 150)
+  }
 
   return (
     <div className='w-full h-full flex'>
@@ -15,17 +34,19 @@ export default function Layout({ children, contentClass = '' }: { children: any;
         <button className={'h-32 quiet-btn flex items-center text-4xl pl-12'}>
           <FontAwesomeIcon
             icon={faChevronLeft}
-            onClick={() => setNavOpen(!navOpen)}
-            className={'transform-gpu duration-300 ' + (navOpen ? 'rotate-0' : 'rotate-180')}
+            onClick={toggleNavOpen}
+            className={`transform-gpu duration-${transitionDurationMs * 2} ` + (navOpen ? 'rotate-0' : 'rotate-180')}
           />
         </button>
       )}
       <section
         className={
-          'h-screen overflow-scroll transition-all p-12 ' + contentClass + (isMobile && navOpen ? ' -mr-56' : '')
+          `h-screen overflow-scroll transition-all duration-${transitionDurationMs} p-12 ` +
+          contentClass +
+          (isMobile && navOpen ? ' -mr-56' : '')
         }
       >
-        {children}
+        <LayoutContext.Provider value={{ navOpen, isTransitioning }}>{children}</LayoutContext.Provider>
       </section>
     </div>
   )
--- components/SeriesGraph/index.tsx
+++ components/SeriesGraph/index.tsx
@@ -6,6 +6,7 @@ import { reviews, accentColors } from '../../constants'
 import { makeDataPoints, clipToPeriod, scrollToSeries } from './util'
 import { seriesId } from '../../util'
 import styles from './styles.module.css'
+import { LayoutContext } from 'components/Layout'
 
 const allDataPoints = makeDataPoints(reviews.finishedReading)
 const allSeriesNames = [...new Set(reviews.finishedReading.map((r) => r.book.series?.name))]
@@ -26,50 +27,58 @@ export default function SeriesGraph() {
 
   return (
     <article className='h-48 mb-16 w-full'>
-      <ResponsiveContainer debounce={200}>
-        <AreaChart data={points} margin={{ top: 0, right: 0, bottom: 0, left: 0 }}>
-          <defs>
-            {allSeriesNames.map((seriesName, i) => (
-              <linearGradient id={`${seriesId(seriesName)}-gradient`} key={i} x1='0' y1='0' x2='0' y2='1'>
-                <stop offset='10%' stopColor={getAreaColor(i)} stopOpacity={1} />
-                <stop offset='100%' stopColor={getAreaColor(i)} stopOpacity={0} />
-              </linearGradient>
-            ))}
-          </defs>
-          <XAxis
-            dataKey='yearMonth'
-            axisLine={false}
-            tickLine={false}
-            allowDataOverflow={false}
-            minTickGap={100}
-            tickFormatter={(value: string) => value.split('-')[0]}
-          />
-          <Tooltip content={ExcludeZeroesTooltip} wrapperClassName='card' />
-          {allSeriesNames.map((seriesName, i) => (
-            <Area
-              key={i}
-              type='monotone'
-              dataKey={seriesName}
-              stroke={getAreaColor(i)}
-              fillOpacity={1}
-              fill={`url(#${seriesId(seriesName)}-gradient)`}
-              isAnimationActive={false}
-              onClick={() => scrollToSeries(seriesName)}
-            />
-          ))}
-        </AreaChart>
-      </ResponsiveContainer>
-      <div className={styles['period-select']}>
-        {Object.entries(periods).map(([key, value]) => (
-          <button
-            key={key}
-            onClick={() => setPeriod(value)}
-            className={'quiet-btn font-thin ' + (value === selectedPeriod ? styles.selected : '')}
-          >
-            {key}
-          </button>
-        ))}
-      </div>
+      <LayoutContext.Consumer>
+        {({ isTransitioning }) =>
+          !isTransitioning ? (
+            <>
+              <ResponsiveContainer>
+                <AreaChart data={points} margin={{ top: 0, right: 0, bottom: 0, left: 0 }}>
+                  <defs>
+                    {allSeriesNames.map((seriesName, i) => (
+                      <linearGradient id={`${seriesId(seriesName)}-gradient`} key={i} x1='0' y1='0' x2='0' y2='1'>
+                        <stop offset='10%' stopColor={getAreaColor(i)} stopOpacity={1} />
+                        <stop offset='100%' stopColor={getAreaColor(i)} stopOpacity={0} />
+                      </linearGradient>
+                    ))}
+                  </defs>
+                  <XAxis
+                    dataKey='yearMonth'
+                    axisLine={false}
+                    tickLine={false}
+                    allowDataOverflow={false}
+                    minTickGap={100}
+                    tickFormatter={(value: string) => value.split('-')[0]}
+                  />
+                  <Tooltip content={ExcludeZeroesTooltip} wrapperClassName='card' />
+                  {allSeriesNames.map((seriesName, i) => (
+                    <Area
+                      key={i}
+                      type='monotone'
+                      dataKey={seriesName}
+                      stroke={getAreaColor(i)}
+                      fillOpacity={1}
+                      fill={`url(#${seriesId(seriesName)}-gradient)`}
+                      isAnimationActive={false}
+                      onClick={() => scrollToSeries(seriesName)}
+                    />
+                  ))}
+                </AreaChart>
+              </ResponsiveContainer>
+              <div className={styles['period-select']}>
+                {Object.entries(periods).map(([key, value]) => (
+                  <button
+                    key={key}
+                    onClick={() => setPeriod(value)}
+                    className={'quiet-btn font-thin ' + (value === selectedPeriod ? styles.selected : '')}
+                  >
+                    {key}
+                  </button>
+                ))}
+              </div>
+            </>
+          ) : undefined
+        }
+      </LayoutContext.Consumer>
     </article>
   )
 }
